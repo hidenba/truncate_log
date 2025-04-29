aca39ceb46c940898a76037fe5209ddb9d7a8f8b
HIDEKUNI Kajita <hide.nba@gmail.com>
2025-04-29 17:22:16 +0900

    first commit
    bundle gem .

---
+++ lib/truncate_log.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "truncate_log/version"
+
+module TruncateLog
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/truncate_log/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TruncateLog
+  VERSION = "0.1.0"
+end
